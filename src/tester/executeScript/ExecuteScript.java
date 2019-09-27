2241968e13384dc1edcc26cf3052e5bd9e5dc76b
Luca Furlano <furlanoluca@libero.it>
2019-09-27 17:04:19 +0200

    minor bug fix

--- src/tester/executeScript/ExecuteScript.java
+++ src/tester/executeScript/ExecuteScript.java
@@ -136,6 +136,7 @@ public class ExecuteScript{
 			output += "\n";
 		}
 		String bufferObject = (String) gvbuffer.getObject();
+		bufferObject = bufferObject==null?"":bufferObject;
 		if(bufferObject.contains("\n")) {
 			output += "> GVBuffer (multiline view): \n" + bufferObject + "\n";
 		} else {
@@ -145,6 +146,7 @@ public class ExecuteScript{
 		output += "> Properties:" + "\n";
 		for(String key:gvbuffer.getPropertyNames()) {
 			String propertyValue = gvbuffer.getProperty(key);
+			propertyValue = propertyValue==null?"":propertyValue;
 			if(propertyValue.contains("\n")) {
 				output += "    > " + key + " (multiline view): \n" + propertyValue + "\n";
 			} else {
