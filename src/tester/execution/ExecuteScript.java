714d081b95bcc24ffa7dcc605d828585ae88a6d4
Luca Furlano <furlanoluca@libero.it>
2019-10-03 10:48:17 +0200

    properties on json files

--- src/tester/execution/ExecuteScript.java
+++ src/tester/execution/ExecuteScript.java
@@ -3,11 +3,14 @@ package tester.execution;
 import static tester.execution.Paths.*;
 import static tester.settings.Constants.*;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.HashMap;
+
+import org.json.JSONObject;
 
 import it.greenvulcano.gvesb.buffer.GVBuffer;
 import it.greenvulcano.gvesb.buffer.GVException;
@@ -39,6 +42,62 @@ public class ExecuteScript{
 	////////////////////////////////////////////////
 
 	private static void initializeTest(GVBuffer data, HashMap<String,GVBuffer> environment) throws GVException, IOException {
+
+		String objectPath = GV_FILE_BUFFER_NAME;
+		String propertyPath = GV_DATA_BUFFER_PROPERTIES_NAME;
+		readBufferFromFile(data, objectPath, propertyPath);
+
+		boolean bufferFileExists = true;
+		int count = 1;
+		while(bufferFileExists) {
+			try {
+				String envObjectPath = "scriptTester/ENV-" + count + "-GVBuffer.txt";
+				String envPropertiesPath = "scriptTester/ENV-" + count + "-Properties.json";
+				GVBuffer envBuffer = new GVBuffer();
+				String bufferName = readBufferFromFile(envBuffer, envObjectPath, envPropertiesPath);
+				if (bufferName != null && !bufferName.equals("")) {
+					environment.put(bufferName,envBuffer);
+				}
+			} catch (IOException e) {
+				bufferFileExists = false;
+			}
+			count++;
+		}
+
+	}
+
+	private static String readBufferFromFile(GVBuffer gvbuffer, String objectPath, String propertyPath)
+			throws GVException, IOException {
+
+		gvbuffer.setObject(new String(Files.readAllBytes(Paths.get(objectPath))));
+		String fileContent = new String(Files.readAllBytes(Paths.get(propertyPath)));
+		JSONObject json = null;
+		try {
+			json = new JSONObject(fileContent);
+			if(!json.has("bufferName")) {
+				System.out.println("ERROR: missing mandatory field 'bufferName' in " + propertyPath);
+				return null;
+			}
+			if(json.has("properties")) {
+				for(Object obj : json.getJSONArray("properties")) {
+					JSONObject propertyPair = (JSONObject) obj;
+					gvbuffer.setProperty(propertyPair.getString("name"), propertyPair.getString("value"));
+				}
+			} else {
+				System.out.println("ERROR: missing mandatory field 'properties' in " + propertyPath);
+			}
+		} catch (Exception e) {
+			System.out.println("ERROR: unable to read gv-script-tester configuration file: " + propertyPath);
+			e.printStackTrace();
+		}
+
+		return json.getString("bufferName");
+
+	}
+
+	private static void initializeTest_old(GVBuffer data, HashMap<String,GVBuffer> environment) throws GVException, IOException {
+
+		////////////////////////////////
 
 		data.setObject(new String(Files.readAllBytes(Paths.get(GV_FILE_BUFFER_NAME))));
 		if(PROPERTY_NAME_1.length()!=0) data.setProperty(PROPERTY_NAME_1, PROPERTY_VALUE_1);
