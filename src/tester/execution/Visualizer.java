b74fd83c117829fbb07e7c84183eb91d0106e16f
Luke460 <furlanoluca@libero.it>
2019-10-05 13:50:15 +0200

    minor visualization improvement and code refactoring

---
+++ src/tester/execution/Visualizer.java
@@ -0,0 +1,69 @@
+package tester.execution;
+
+import static tester.execution.Paths.LOG_FILE_PATH;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+import it.greenvulcano.gvesb.buffer.GVBuffer;
+
+public class Visualizer {
+	
+	public static void printGVBuffer(GVBuffer gvbuffer) {
+		String output = generateBufferInfo(gvbuffer, null);
+		System.out.print(output);
+	}
+
+	public static void writeInTheLog(GVBuffer gvbuffer, String bufferName) throws IOException {
+		String output = generateBufferInfo(gvbuffer, bufferName);
+		Files.write(Paths.get(LOG_FILE_PATH), output.getBytes(), StandardOpenOption.APPEND);
+	}
+
+	public static String generateBufferInfo(GVBuffer gvbuffer, String bufferName) {
+		String output = "";
+		output += "---------------- GV BUFFER --------------------\n";
+		output += "\n";
+		if(bufferName!=null) {
+			output += "> Name = " + bufferName + "\n";
+			output += "\n";
+		}
+		String bufferObject = (String) gvbuffer.getObject();
+		output = printElement(output, bufferObject, "Object");
+		output += "\n";
+
+		if(gvbuffer.getPropertyNames().length<1) {
+			output += "> No Properties" + "\n";
+		} else {
+			output += "> Properties:" + "\n";
+			for(String key:gvbuffer.getPropertyNames()) {
+				String propertyValue = gvbuffer.getProperty(key);
+				output += "    ";
+				output = printElement(output, propertyValue, key);
+			}
+		}
+		output += "-----------------------------------------------" + "\n\n";
+		return output;
+	}
+
+	private static String printElement(String output, String element, String elementName) {
+		if(element!=null && element.contains("\n")) {
+			output += "> " + elementName + " (multiline view):\n" + element + "\n";
+		} else {
+			String info = null;
+			if(element==null) {
+				info = "is null";
+			} else if (element.equals("")){
+				info = "is empty";
+			}
+			if(info==null) {
+				output += "> " + elementName + " = " + element + "\n";
+			} else {
+				output += "> " + elementName + " " + info + "\n";
+			}	
+		}
+		return output;
+	}
+
+}
