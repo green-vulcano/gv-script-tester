44388541c778e783f2deac76f426ed8bce70d066
Luca Furlano <furlanoluca@libero.it>
2019-12-03 11:30:56 +0100

    improved Groovy example

--- src/tester/groovy/GroovyScript.java
+++ src/tester/groovy/GroovyScript.java
@@ -11,9 +11,22 @@ public class GroovyScript {
 		///////////////////////////////  SCRIPT AREA  ///////////////////////////////
 
 		// example 2 - Groovy script
-		org.json.JSONObject inputJson = new org.json.JSONObject(data.getObject());
-		String city = inputJson.getString("city").toUpperCase();
-	    data.setProperty("CITY", city);
+
+		String inputJsonString = data.getObject().toString();
+		
+		org.json.JSONObject inputJson = new org.json.JSONObject(inputJsonString);
+		
+		int number = inputJson.getJSONObject("player").getInt("number");
+		String name = inputJson.getJSONObject("player").getString("name");
+		String surname = inputJson.getJSONObject("player").getString("surname");
+		
+		String code = (name + surname + number).toUpperCase();
+	
+		org.json.JSONObject outputJson = new org.json.JSONObject();
+		outputJson.put("number", number);
+		outputJson.put("code", code);
+		
+		data.setProperty("OUTPUT_JSON", outputJson.toString());		         
 
 		/////////////////////////////////////////////////////////////////////////////
 
--- src/tester/settings/Constants.java
+++ src/tester/settings/Constants.java
@@ -4,7 +4,7 @@ public interface Constants {
 	
 	//// Execution settings
 
-	public static final boolean IS_JAVASCRIPT = true;  // false -> Groovy script  |  true -> Javascript
+	public static final boolean IS_JAVASCRIPT = false;  // false -> Groovy script  |  true -> Javascript
 	public static final boolean IS_FUNCTION = false;  // false -> script  |  true -> function
 	
 	
