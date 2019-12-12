610cb473b900e6fe7a2a0abc4981ae5258df64ef
Luca Furlano <furlanoluca@libero.it>
2019-12-12 09:48:23 +0100

    code cleaning

---
+++ src/tester/execution/configuration/ScriptTesterProperties.java
@@ -0,0 +1,9 @@
+package tester.execution.configuration;
+
+public interface ScriptTesterProperties {
+	
+	// Script Tester Properties
+	
+	public static final String UNDEFINED_SCRIPT_TESTER_PROPERTY = "[script-tester-property]:undefined";
+	
+}
